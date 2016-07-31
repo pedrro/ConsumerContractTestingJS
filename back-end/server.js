6dfe450b76a51b50345eae084245d9eba241d742
pedro silva <pedrro2@hotmail.com>
2016-07-31 15:15:14 -0300

    resolving issue of CORS

--- back-end/server.js
+++ back-end/server.js
@@ -5,6 +5,11 @@ var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
