1cef79e04e192ec229eb3b8d1b022b42f783d5b9
pedro silva <pedrro2@hotmail.com>
2016-09-06 21:44:00 -0300

    contract test

---
+++ test/contract/contract_spec.js
@@ -0,0 +1,18 @@
+var Contract = require('consumer-contracts').Contract;
+var Joi = require('consumer-contracts').Joi;
+
+module.exports = new Contract({
+    name: 'find all students',
+    consumer: 'test',
+    request: {
+        method: 'GET',
+        url: 'http://localhost:3000/students'
+    },
+    response: {
+        statusCode: 200,
+        body: Joi.array().items(Joi.object().keys({
+            name: Joi.string(),
+            age: Joi.number().integer()
+        }))
+    }
+});
