03e4e000566942ea25d9420c4cbc6cd9d36d60c8
pedro silva <pedrro2@hotmail.com>
2016-09-07 21:17:53 -0300

    add validation of schema on back-end

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var util = require('util');
+var expressValidator = require('express-validator');
 var StudentsController = require('./controller/studentsController.js');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,14 +12,15 @@ var studentsController = new StudentsController();
 console.log(studentsController);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
-
-
+app.use(expressValidator());
 
 /* ROUTES */
 var studentsRoute = router.route('/students');
@@ -24,19 +28,42 @@ var studentRoute = router.route('/student/:id');
 
 /* END-POINT */
 studentsRoute.get(function(request, response) {
-  response.json(studentsController.findAll());
+    response.json(studentsController.findAll());
 });
 
 studentsRoute.post(function(request, response) {
-  var student = {
-    name: request.body.name,
-    age: parseInt(request.body.age)
-  };
-  response.json(studentsController.create(student));
+    var student = {
+        'name': {
+            notEmpty: true,
+            errorMessage: 'Invalid name'
+        },
+        'age': {
+            notEmpty: true,
+            isLength: {
+                options: [{
+                    min: 1,
+                    max: 2
+                }],
+                errorMessage: 'Must be between 1 and 2 chars long' // Error message for the validator, takes precedent over parameter message
+            },
+            errorMessage: 'Invalid age'
+        }
+    };
+
+    request.checkBody(student);
+    if(request.validationErrors()) {
+        response.status(400).send('errors on request: ' + util.inspect(request.validationErrors()));
+    }else {
+        response.json(studentsController.create(buildStudent(request.body.name, parseInt(request.body.age))));
+    }
 });
 
+var buildStudent = function (name, age) {
+    return {'name' : name, 'age' : age};
+};
+
 studentRoute.get(function(request, response) {
-  response.json(studentsController.getById(request.params.id));
+    response.json(studentsController.getById(request.params.id));
 });
 
 
