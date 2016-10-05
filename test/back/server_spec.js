74732fe1e5ccff099441fdfefcad87fd8f96fbfa
pedro silva <pedrro2@hotmail.com>
2016-10-04 22:32:04 -0300

    update request on back-end

--- test/back/server_spec.js
+++ test/back/server_spec.js
@@ -4,19 +4,19 @@ frisby.create('Should return the list of students')
   .get('http://localhost:3000/students')
   .expectStatus(200)
   .expectJSON([
-    {name: 'Pedro', age: 10},
-    {name: 'João', age: 11},
-    {name: 'Thiago', age: 9}
+    {nome: 'Pedro', idade: 10},
+    {nome: 'João', idade: 11},
+    {nome: 'Thiago', idade: 9}
   ])
   .toss();
 
 frisby.create('Should add a new student')
   .post('http://localhost:3000/students',
-    {name: 'José', age: 12}
+    {nome: 'José', idade: 12}
   )
   .expectStatus(200)
   .expectJSON(
-    {name: 'José', age: 12}
+    {nome: 'José', idade: 12}
 )
   .toss();
 
@@ -24,6 +24,6 @@ frisby.create('Should get a specific student')
   .get('http://localhost:3000/student/1')
   .expectStatus(200)
   .expectJSON(
-    {name: 'Pedro', age: 10}
+    {nome: 'Pedro', idade: 10}
   )
   .toss();
